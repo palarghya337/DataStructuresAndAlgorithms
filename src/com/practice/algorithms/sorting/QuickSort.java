0ac72399b696bd4322000b619ca5871b5d6c9670
Arghya Pal <palarghya337@gmail.com>
2020-03-22 13:49:28 +0530

    Added different sorting techniques

---
+++ src/com/practice/algorithms/sorting/QuickSort.java
@@ -0,0 +1,15 @@
+package com.practice.algorithms.sorting;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		
+		int array[] = null;
+		sort(array);
+	}
+
+	private static void sort(int[] array) {
+		
+		int length = array.length;
+	}
+}
---
+++ src/com/practice/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,5 @@
+package com.practice.algorithms.sorting;
+
+public class SelectionSort {
+
+}
