d56b425a1f84d7aec255a13a5cfcbf930ae4c90e
Arghya Pal <palarghya337@gmail.com>
2020-03-30 14:56:56 +0530

    implemented the process of insertsion sort

--- src/com/practice/algorithms/sorting/InsertsionSort.java
+++ src/com/practice/algorithms/sorting/InsertsionSort.java
@@ -1,5 +1,48 @@
 package com.practice.algorithms.sorting;
 
+import java.util.Arrays;
+
 public class InsertsionSort {
 
+	public static void main(String[] args) {
+		
+		int array[] = {10, 16, 18, 2, 15, 6, 3, 9, 5};
+		/*
+		Random random = new Random();
+		array = IntStream.generate(() -> random.nextInt(40))
+				.distinct()
+				.limit(10)
+				.toArray();
+		*/
+		System.out.println(Arrays.toString(array));
+		sort(array);
+	}
+
+	private static void sort(int[] array) {
+		
+		int arrayLength = array.length;
+		for (int i = 0; i < arrayLength - 1; i++) {
+			
+			int small = array[i + 1];
+			if (array[i] > small) {
+				/*
+				 * if element of index i is greater than element
+				 * of index (i + 1) then move all the previous
+				 * i'th element to (i + 1)'th element. This move
+				 * will happen till it finds the smaller element
+				 * or it reaches the first position. Then copy
+				 * the element to the last found (index + 1)
+				 * position.
+				 **/
+				int j = i;
+				while (j >= 0 && array[j] > small) {
+					
+					array[j + 1] = array[j];
+					j--;
+				}
+				array[j + 1] = small;
+				System.out.println(Arrays.toString(array));
+			}
+		}
+	}
 }
